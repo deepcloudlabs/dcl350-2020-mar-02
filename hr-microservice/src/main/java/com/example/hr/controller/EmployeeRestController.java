852eceec77add0a406e18c007cbda56719db974a
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-03 10:45:18 +0300

    implement Put/Post/Patch REST Api methods

--- hr-microservice/src/main/java/com/example/hr/controller/EmployeeRestController.java
+++ hr-microservice/src/main/java/com/example/hr/controller/EmployeeRestController.java
@@ -1,13 +1,22 @@
 package com.example.hr.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 
+import com.example.hr.dto.UpdateEmployeeRequest;
 import com.example.hr.entity.Employee;
 import com.example.hr.service.EmployeeService;
 
@@ -17,16 +26,33 @@ import com.example.hr.service.EmployeeService;
 @RequestMapping("employees")
 @CrossOrigin
 public class EmployeeRestController {
-	@Autowired 
+	@Autowired
 	private EmployeeService empSrv;
-    // /hr/api/v1/employees/12345678910
+
+	// /hr/api/v1/employees/12345678910
 	@GetMapping("{identity}")
-	public Employee findEmployeeByIdentity(
-			@PathVariable
-			String identity) {
+	public Employee findEmployeeByIdentity(@PathVariable String identity) {
 		return empSrv.findByIdentity(identity);
 	}
+
+	// /hr/api/v1/employees?page=10&size=25
+	@GetMapping
+	public List<Employee> findEmployees(@RequestParam int page, @RequestParam int size) {
+		return empSrv.findEmployees(page, size);
+	}
+
+	@PostMapping
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return empSrv.addEmployee(employee);
+	}
+
+	@PutMapping("{identity}")
+	public Employee updateEmployee(@RequestBody UpdateEmployeeRequest employee, @PathVariable String identity) {
+		return empSrv.updateEmployee(employee, identity);
+	}
+
+	@PatchMapping("{identity}")
+	public Employee patchEmployee(@RequestBody Map<String, Object> employee, @PathVariable String identity) {
+		return empSrv.patchEmployee(employee, identity);
+	}
 }
-
-
-
--- hr-microservice/src/main/java/com/example/hr/service/EmployeeService.java
+++ hr-microservice/src/main/java/com/example/hr/service/EmployeeService.java
@@ -1,18 +1,80 @@
 package com.example.hr.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.hr.dto.UpdateEmployeeRequest;
+import com.example.hr.entity.Department;
 import com.example.hr.entity.Employee;
 import com.example.hr.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+	private static final String[] UPDATABLE_FIELDS = { "salary", "iban", "photo", "department", "fulltime" };
 	@Autowired
 	private EmployeeRepository empRepo;
+
 	public Employee findByIdentity(String identity) {
-		// TODO Auto-generated method stub
-		return null;
+		return empRepo.findById(identity).orElseThrow(() -> new IllegalArgumentException("Cannot find employee"));
+	}
+
+	public List<Employee> findEmployees(int page, int size) {
+		return empRepo.findAll(PageRequest.of(page, size)).getContent();
+	}
+
+	@Transactional
+	public Employee addEmployee(Employee employee) {
+		String identity = employee.getIdentity();
+		if (empRepo.existsById(identity)) {
+			throw new IllegalArgumentException("Employee already exists!");
+		}
+		return empRepo.save(employee);
+	}
+
+	@Transactional
+	public Employee updateEmployee(UpdateEmployeeRequest employee, String identity) {
+		Optional<Employee> emp = empRepo.findById(identity);
+		if (!emp.isPresent())
+			throw new IllegalArgumentException("Cannot find employee to update");
+		Employee managed = emp.get();
+		managed.setIban(employee.getIban());
+		managed.setSalary(employee.getSalary());
+		managed.setFulltime(employee.isFulltime());
+		managed.setPhoto(employee.getPhoto());
+		managed.setDepartment(employee.getDepartment());
+		return managed;
+	}
+
+	@Transactional
+	public Employee patchEmployee(Map<String, Object> employee, String identity) {
+		Optional<Employee> emp = empRepo.findById(identity);
+		if (!emp.isPresent())
+			throw new IllegalArgumentException("Cannot find employee to patch");
+		Employee managed = emp.get();
+		if (employee.containsKey("department"))
+			employee.put("department", Department.valueOf(employee.get("department").toString()));
+		Arrays.stream(UPDATABLE_FIELDS).forEach(field -> {
+			if (employee.containsKey(field)) {
+				Object value = employee.get(field);
+				try {
+					Field managedField = Employee.class.getDeclaredField(field);
+					managedField.setAccessible(true);
+					managedField.set(managed, value);
+					managedField.setAccessible(false);
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		});
+		return managed;
 	}
 
 }
