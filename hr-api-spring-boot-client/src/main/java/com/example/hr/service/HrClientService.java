78599199665fef26f8d7d7233cc3bcd8b252d328
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-05 10:06:55 +0300

    client-side load balancing raw version

--- hr-api-spring-boot-client/src/main/java/com/example/hr/service/HrClientService.java
+++ hr-api-spring-boot-client/src/main/java/com/example/hr/service/HrClientService.java
@@ -1,20 +1,45 @@
 package com.example.hr.service;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class HrClientService {
-	private static final String URL = 
-		     "http://localhost:5100/hr/api/v1"
-			+ "/employees?page=0&size=10";
-	@Scheduled(fixedRate = 10_000)
+	private static final String URL = "http://%s:%d/hr/api/v1/employees?page=0&size=10";
+	@Autowired 
+	private DiscoveryClient discoveryClient;
+	private List<ServiceInstance> instances;
+	private AtomicInteger counter = 
+			new AtomicInteger();
+	@PostConstruct
+	public void init() {
+		// spring.application.name
+		instances = discoveryClient.getInstances("hr");		  
+	    instances.forEach( instance -> {
+	    	System.err.println(instance.getHost()
+	    			+ ":" + instance.getPort());
+	    });
+	}
+	
+	@Scheduled(fixedRate = 1_000)
 	public void callHrApi() {
 		RestTemplate rt = new RestTemplate();
-		String employees = 
-				rt.getForEntity(URL, String.class)
-				  .getBody();
+		int index = counter.getAndIncrement() % instances.size();
+		ServiceInstance instance = 
+				      instances.get(index);
+		String requestUrl = String.format(URL, 
+			instance.getHost(),instance.getPort());
+		String employees = rt.getForEntity(
+				requestUrl, String.class).getBody();
 		System.out.println(employees);
 	}
 }
