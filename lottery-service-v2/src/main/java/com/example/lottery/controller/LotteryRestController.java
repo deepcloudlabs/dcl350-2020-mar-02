61abe422931dd03f4cdc0ded4efb4c8fc6466047
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-06 13:50:35 +0300

    initial commit

---
+++ lottery-service-v2/src/main/java/com/example/lottery/controller/LotteryRestController.java
@@ -0,0 +1,26 @@
+package com.example.lottery.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lottery.service.LotteryService;
+
+@RestController
+@RequestMapping("numbers")
+public class LotteryRestController {
+	private LotteryService lotteryService;
+	
+	public LotteryRestController(LotteryService lotteryService) {
+		this.lotteryService = lotteryService;
+	}
+    // localhost:6100/lottery/api/v1/numbers?n=10
+	@GetMapping
+	public List<List<Integer>> getNumbers(
+			@RequestParam int n){
+		return lotteryService.draw(n);
+	}
+}
