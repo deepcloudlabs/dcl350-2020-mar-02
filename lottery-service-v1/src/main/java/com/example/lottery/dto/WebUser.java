31d6c69c43f48d2db9033768a23acc288c7cab76
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-06 16:30:09 +0300

    security

---
+++ lottery-service-v1/src/main/java/com/example/lottery/dto/WebUser.java
@@ -0,0 +1,35 @@
+package com.example.lottery.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class WebUser {
+	@NotNull
+	private String username;
+	@NotNull
+	private String password;
+
+	public WebUser() {
+	}
+
+	public WebUser(@NotNull String username, @NotNull String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
